0bff1f9db0ded26e9557295ac05319c6034e66f5
Erwin Mueller <erwin.mueller@deventm.org>
2016-01-24 15:43:15 +0100

    Create Eclipse Vaadin project

---
+++ geophylo/src/com/anrisoftware/geophylo/GeoLayout.java
@@ -0,0 +1,10 @@
+package com.anrisoftware.geophylo;
+
+
+public class GeoLayout extends com.vaadin.ui.AbstractComponent {
+
+
+	public GeoLayout() {
+	}
+
+}
--- src/main/webapp/VAADIN/widgetsets/com.anrisoftware.geophylo.MyAppWidgetset/deferredjs/05C083F75743F6384A4FA0FBB3A5FCD0/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_anrisoftware_geophylo_MyAppWidgetset.runAsyncCallback2('Bbb(1540,1,cTd);_.tc=function Wac(){gZb((!_Yb&&(_Yb=new lZb),_Yb),this.a.d)};GMd(Th)(2);\n//# sourceURL=com.anrisoftware.geophylo.MyAppWidgetset-2.js\n')
